b2fe0636757857f9f6f53e15612378b01ad97127
Tirq <tiagogelatti@outlook.com>
2018-05-16 14:11:04 -0300

    Initial commit.

---
+++ src/main/java/com/tirq/springbootguru/SpringBootGuruApplication.java
@@ -0,0 +1,12 @@
+package com.tirq.springbootguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGuruApplication.class, args);
+	}
+}
